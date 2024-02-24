a8c0abd964d365348e9c48a2a9702b831b5578a1
DevGeok <georgevergel@gmail.comgit config --global user.email georgevergel@gmail.com>
2024-02-24 15:03:32 -0500

    sin mode dev

--- front/webpack.config.cjs
+++ front/webpack.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: '[name].js'
   },
-  mode: "development", // Change to "production" when ready to deploy
+  //mode: "development", // Change to "production" when ready to deploy
   devtool: 'eval-source-map',
   // ... the rest of your webpack configuration
 };
