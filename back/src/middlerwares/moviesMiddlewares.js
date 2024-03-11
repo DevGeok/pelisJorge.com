48a23b2fde994c440228aa55dd9c5cb0f0ccbcd3
DevGeok <georgevergel@gmail.comgit config --global user.email georgevergel@gmail.com>
2024-03-10 21:12:37 -0500

    Entregable con errores con catchAsync

--- back/src/middlerwares/moviesMiddlewares.js
+++ back/src/middlerwares/moviesMiddlewares.js
@@ -27,34 +27,34 @@ module.exports = {
            let errorMessage;
 
            if(year.toString().length > 4) {
-                errorMessage = "ERROR: El año no puede tener más de 4 digitos";
+                errorMessage = "Mensaje del BackEnd. ERROR: El año no puede tener más de 4 digitos";
                 res.status(400).json({message: errorMessage});
-                return next(new Error(errorMessage));
+                return console.error("ERROR: El año no puede tener más de 4 digitos");
             }
            
            if(year<1895){
             errorMessage = `Mensaje del BackEnd. ERROR: La primera película se lanzó en 1985 y fue  "La salida de los obreros de la fábrica Lumière", el año de tu película no puede ser menor a 1,985`;
             res.status(400).json({message:errorMessage});
-            return next(new Error(errorMessage));
+            return console.error(`ERROR: La primera película se lanzó en 1985 y fue  "La salida de los obreros de la fábrica Lumière", el año de tu película no puede ser menor a 1,985`)
             }
 
            if(rate>10) {
             errorMessage ="Mensaje del BackEnd. ERROR: La calificación es del 1 al 10, por mucho que te guste la película no puede tener una calificación mayor a 10";
            res.status(400).json({message:errorMessage});
-           return next(new Error(errorMessage));;
+           return console.error("ERROR: La calificación es del 1 al 10, por mucho que te guste la película no puede tener una calificación mayor a 10");
           }
            
 
            if(rate.toString().length>4){
             errorMessage = "Mensaje del BackEnd. ERROR: Conservemos la calificación simple, 2 decimales es más que suficiente para ser específicos, no seamos piky";
             res.status(400).json({message:errorMessage});
-            return next(new Error(errorMessage));
+            return console.error("ERROR: Conservemos la calificación simple, 2 decimales es más que suficiente para ser específicos, no seamos piky");
            };
 
            if(!isUrl(poster)){
             errorMessage = "Mensaje del BackEnd. ERROR: El URL de la imagen no es válido";
             res.status(400).json({message:errorMessage});
-            return next(new Error(errorMessage));
+            return console.error("ERROR: El URL de la imagen no es válido");
            };
 
            let movieExist = await Movies.findOne({"title": title});
@@ -62,7 +62,7 @@ module.exports = {
            if(movieExist !== null){
             errorMessage = "Mensaje del BackEnd. ERROR: Ya existe esta película en la base de datos";
             res.status(400).json({message:errorMessage});
-            return next(new Error(errorMessage));
+            return console.error("ERROR: Ya existe esta película en la base de datos")
           };
           console.log("AddMovies middleWare STATUS: PASSED");
         };
