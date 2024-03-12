8a1351a18dcaddc62d0b252024c4057dabb72593
DevGeok <georgevergel@gmail.comgit config --global user.email georgevergel@gmail.com>
2024-03-12 10:04:22 -0500

    Últimos detalles, Todos los datos de movies agregados y link ancla en título

--- back/src/middlerwares/moviesMiddlewares.js
+++ back/src/middlerwares/moviesMiddlewares.js
@@ -2,70 +2,63 @@ const Movies = require("../models/Movies");
 const catchAsync = require("../utils/catchAsync");
 
 function isUrl(string) {
-    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocolo
-        '((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,})' + // nombre de dominio
-        '(\\:\\d+)?(\\/[-a-zA-Z\\d%@_.~+&:;=]*)*' + // puerto, ruta (incluyendo '@')
-        '(\\?[;&a-zA-Z\\d%@_.~+=-]*)?' + // consulta (incluyendo '@')
-        '(\\#[-a-zA-Z\\d_]*)?$', 'i'); // fragmento
-    return !!pattern.test(string);
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocolo
+      "((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,})" + // nombre de dominio
+      "(\\:\\d+)?(\\/[-a-zA-Z\\d%@_.~+&:;=]*)*" + // puerto, ruta (incluyendo '@')
+      "(\\?[;&a-zA-Z\\d%@_.~+=-]*)?" + // consulta (incluyendo '@')
+      "(\\#[-a-zA-Z\\d_]*)?$",
+    "i"
+  ); // fragmento
+  return !!pattern.test(string);
 }
 
 module.exports = {
-    addMovieMiddleware : async (req, res, next) => {
-        let {
-            title,
-            year,
-            director,
-            duration,
-            genre,
-            rate,
-            poster
-        } 
-        = req.body;
+  addMovieMiddleware: async (req, res, next) => {
+    let { title, year, director, duration, genre, rate, poster } = req.body;
 
-        if(title && year && director && duration && genre && rate && poster){
-           let errorMessage;
+    if (title && year && director && duration && genre && rate && poster) {
+      let errorMessage;
 
-           if(year.toString().length > 4) {
-                errorMessage = "Mensaje del BackEnd. ERROR: El año no puede tener más de 4 digitos";
-                res.status(400).json({message: errorMessage});
-                return console.error("ERROR: El año no puede tener más de 4 digitos");
-            }
-           
-           if(year<1895){
-            errorMessage = `Mensaje del BackEnd. ERROR: La primera película se lanzó en 1985 y fue  "La salida de los obreros de la fábrica Lumière", el año de tu película no puede ser menor a 1,985`;
-            res.status(400).json({message:errorMessage});
-            return console.error(`ERROR: La primera película se lanzó en 1985 y fue  "La salida de los obreros de la fábrica Lumière", el año de tu película no puede ser menor a 1,985`)
-            }
+      if (year.toString().length > 4) {
+        errorMessage = "ERROR: El año no puede tener más de 4 digitos";
+        res.status(400).json({ message: `Mensaje del BackEnd. ${errorMessage}`});
+        return console.error(errorMessage);
+      }
 
-           if(rate>10) {
-            errorMessage ="Mensaje del BackEnd. ERROR: La calificación es del 1 al 10, por mucho que te guste la película no puede tener una calificación mayor a 10";
-           res.status(400).json({message:errorMessage});
-           return console.error("ERROR: La calificación es del 1 al 10, por mucho que te guste la película no puede tener una calificación mayor a 10");
-          }
-           
+      if (year < 1895) {
+        errorMessage = `ERROR: La primera película se lanzó en 1985 y fue  "La salida de los obreros de la fábrica Lumière", el año de tu película no puede ser menor a 1,895`;
+        res.status(400).json({ message: `Mensaje del BackEnd. ${errorMessage}`});
+        return console.error(errorMessage);
+      }
 
-           if(rate.toString().length>4){
-            errorMessage = "Mensaje del BackEnd. ERROR: Conservemos la calificación simple, 2 decimales es más que suficiente para ser específicos, no seamos piky";
-            res.status(400).json({message:errorMessage});
-            return console.error("ERROR: Conservemos la calificación simple, 2 decimales es más que suficiente para ser específicos, no seamos piky");
-           };
+      if (rate > 10) {
+        errorMessage = "ERROR: La calificación es del 1 al 10, por mucho que te guste la película no puede tener una calificación mayor a 10";
+        res.status(400).json({ message: `Mensaje del BackEnd. ${errorMessage}`});
+        return console.error(errorMessage);
+      }
 
-           if(!isUrl(poster)){
-            errorMessage = "Mensaje del BackEnd. ERROR: El URL de la imagen no es válido";
-            res.status(400).json({message:errorMessage});
-            return console.error("ERROR: El URL de la imagen no es válido");
-           };
+      if (rate.toString().length > 4) {
+        errorMessage = "ERROR: Conservemos la calificación simple, 2 decimales es más que suficiente para ser específicos, no seamos piky";
+        res.status(400).json({ message: `Mensaje del BackEnd. ${errorMessage}`});
+        return console.error(errorMessage);
+      }
 
-           let movieExist = await Movies.findOne({"title": title});
+      if (!isUrl(poster)) {
+        errorMessage = "ERROR: El URL de la imagen no es válido";
+        res.status(400).json({ message: `Mensaje del BackEnd. ${errorMessage}`});
+        return console.error(errorMessage);
+      }
 
-           if(movieExist !== null){
-            errorMessage = "Mensaje del BackEnd. ERROR: Ya existe esta película en la base de datos";
-            res.status(400).json({message:errorMessage});
-            return console.error("ERROR: Ya existe esta película en la base de datos")
-          };
-          console.log("AddMovies middleWare STATUS: PASSED");
-        };
+      let movieExist = await Movies.findOne({ title: title });
+
+      if (movieExist !== null) {
+        errorMessage = "ERROR: Ya existe esta película en la base de datos";
+        res.status(400).json({ message: `Mensaje del BackEnd. ${errorMessage}`});
+        return console.error(errorMessage);
+      }
+      console.log("AddMovies middleWare STATUS: PASSED");
+    }
     next();
-    },
+  },
 };
--- front/scripts/Index/moviesToHTML.js
+++ front/scripts/Index/moviesToHTML.js
@@ -1,22 +1,25 @@
 //movieToHtml.js
-
+let id = 0;
 export function moviesToHTML(movies){
-        let  movieHTML = document.createElement("section");
-        movieHTML.classList.add("card", "cardSize", "shadow-lg", "p-1", "mx-2", "rounded", "bg-opaco-90", "my-4", "position-relative");
-        let generos = movies.genre.join(" ");
-        movieHTML.innerHTML =      //Agrego template
-        `
-        <h5 class="card-title text-center">
-        <a class="noSubrayado text-monospace" href="${movies.poster}">${movies.title}</a>
-        </h5>
-        <div>
-        <img class="card-img-top" src="${movies.poster}" alt="El poster de ${movies.title}"/>
-        </div>
-         <ul class="card-body list-group p-1 m-1">
+      let movieHTML = document.createElement("section");
+      movieHTML.classList.add("card", "cardSize", "shadow-lg", "p-1", "mx-2", "rounded", "bg-opaco-90", "my-4", "position-relative");
+      let generos = movies.genre.join(", "); // Cambio hecho aquí para separar los géneros por comas.
+      movieHTML.id= `ancla${id}`
+      movieHTML.innerHTML = 
+      `
+      <h5 class="card-title text-center" id="">
+      <a class="noSubrayado text-monospace" href="#ancla${id++}">${movies.title}</a>
+      </h5>
+      <div>
+      <img class="card-img-top" src="${movies.poster}" alt="El poster de ${movies.title}"/>
+      </div>
+      <ul class="card-body list-group p-1 m-1">
+          <li class="list-group-item">Año: <strong>${movies.year}</strong></li>
+          <li class="list-group-item">Director: <strong>${movies.director}</strong></li>
           <li class="list-group-item">Calificación: <strong>${movies.rate}</strong></li>
           <li class="list-group-item">Géneros: <strong>${generos}</strong></li>
           <li class="list-group-item">Duración: <strong>${movies.duration}</strong></li>
-         </ul>
-          `;
-          return movieHTML;
-    };
+      </ul>
+      `;
+      return movieHTML;
+  };
